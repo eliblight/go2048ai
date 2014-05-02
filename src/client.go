e43091ea99b2f38c6fb82e4fa4815a1dd931ce53
Eran Liberty <eran.liberty@gmail.com>
2014-05-02 22:34:22 +0300

    Added get empty

--- src/client.go
+++ src/client.go
@@ -3,30 +3,44 @@ package main
 import (
 	"fmt"
 )
+
 type Board interface {
 	Get(x, y int) int
 	Set(x, y, v int)
 }
 
-func PrintBoard(b Board) {
-	for y := 0 ; y < 4 ; y++ {
-		for x:= 0 ; x < 4 ; x++ {
-			fmt.Print(b.Get(x,y))
+func BoardPrint(b Board) {
+	for y := 0; y < 4; y++ {
+		for x := 0; x < 4; x++ {
+			fmt.Print(b.Get(x, y))
 			if x < 3 {
 				fmt.Print("\t")
+
 			}
 		}
-		if y < 3 {
-			fmt.Println()
-		}
+		fmt.Println()
 	}
 }
 
-type BasicBoard struct {
-	cells [4][4]int 
+func BoardGetEmpty(b Board) [][2]int {
+	var r = make([][2]int, 0, 16)
+	for y := 0; y < 4; y++ {
+		for x := 0; x < 4; x++ {
+			if b.Get(x, y) == 0 {
+				n := len(r)
+				r = r[:n+1]
+				r[n] = [2]int{x, y}
+			}
+		}
+	}
+	return r
 }
 
-func (b *BasicBoard) Get(x, y int) int{
+type BasicBoard struct {
+	cells [4][4]int
+}
+
+func (b *BasicBoard) Get(x, y int) int {
 	return b.cells[x][y]
 }
 
@@ -34,9 +48,9 @@ func (b *BasicBoard) Set(x, y, v int) {
 	b.cells[x][y] = v
 }
 
-
 func main() {
 	var b *BasicBoard = new(BasicBoard)
-	PrintBoard(b)
+	BoardPrint(b)
+	empty := BoardGetEmpty(b)
+	fmt.Println(empty, len(empty))
 }
-
