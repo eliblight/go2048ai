11db51edd6269efdb7469deff5d77d62fc3e8620
Eran Liberty <eran.liberty@gmail.com>
2014-05-03 23:44:34 +0300

    something is working

--- src/client.go
+++ src/client.go
@@ -2,17 +2,68 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 )
 
-type Board interface {
-	Get(x, y int) int
-	Set(x, y, v int)
+func GetStartValue() int {
+	if rand.Intn(10) == 0 {
+		return 4
+	}
+	return 2
 }
 
-func BoardPrint(b Board) {
+type Tuple [2]int
+
+func (t *Tuple) xy() (x, y int) {
+	return t[0], t[1]
+}
+
+type TupleVector []Tuple
+
+func (p *TupleVector) append(t Tuple) {
+	l := len(*p)
+	*p = (*p)[:l+1]
+	(*p)[l] = t
+}
+
+var direction_map = map[string][]TupleVector{
+	"left": {
+		{{0, 0}, {1, 0}, {2, 0}, {3, 0}},
+		{{0, 1}, {1, 1}, {2, 1}, {3, 1}},
+		{{0, 2}, {1, 2}, {2, 2}, {3, 2}},
+		{{0, 3}, {1, 3}, {2, 3}, {3, 3}}},
+	"right": {
+		{{3, 0}, {2, 0}, {1, 0}, {0, 0}},
+		{{3, 1}, {2, 1}, {1, 1}, {0, 1}},
+		{{3, 2}, {2, 2}, {1, 2}, {0, 2}},
+		{{3, 3}, {2, 3}, {1, 3}, {0, 3}}},
+	"up": {
+		{{0, 0}, {0, 1}, {0, 2}, {0, 3}},
+		{{1, 0}, {1, 1}, {1, 2}, {1, 3}},
+		{{2, 0}, {2, 1}, {2, 2}, {2, 3}},
+		{{3, 0}, {3, 1}, {3, 2}, {3, 3}}},
+	"down": {
+		{{0, 3}, {0, 2}, {0, 1}, {0, 0}},
+		{{1, 3}, {1, 2}, {1, 1}, {1, 0}},
+		{{2, 3}, {2, 2}, {2, 1}, {2, 0}},
+		{{3, 3}, {3, 2}, {3, 1}, {3, 0}}}}
+
+type Board struct {
+	cells [4][4]int
+}
+
+func (b *Board) get(x, y int) int {
+	return b.cells[x][y]
+}
+
+func (b *Board) set(x, y, v int) {
+	b.cells[x][y] = v
+}
+
+func (b *Board) print() {
 	for y := 0; y < 4; y++ {
 		for x := 0; x < 4; x++ {
-			fmt.Print(b.Get(x, y))
+			fmt.Print(b.get(x, y))
 			if x < 3 {
 				fmt.Print("\t")
 
@@ -22,35 +73,142 @@ func BoardPrint(b Board) {
 	}
 }
 
-func BoardGetEmpty(b Board) [][2]int {
-	var r = make([][2]int, 0, 16)
+func (b *Board) getEmptyCells() TupleVector {
+	empty_cells := make(TupleVector, 0, 16)
 	for y := 0; y < 4; y++ {
 		for x := 0; x < 4; x++ {
-			if b.Get(x, y) == 0 {
-				n := len(r)
-				r = r[:n+1]
-				r[n] = [2]int{x, y}
+			if b.get(x, y) == 0 {
+				empty_cells.append(Tuple{x, y})
 			}
 		}
 	}
-	return r
+	return empty_cells
 }
 
-type BasicBoard struct {
-	cells [4][4]int
+func (b *Board) getRandomEmtpyCell() (int, int) {
+	empty_cells := b.getEmptyCells()
+	cell := empty_cells[rand.Intn(len(empty_cells))]
+	return cell[0], cell[1]
 }
 
-func (b *BasicBoard) Get(x, y int) int {
-	return b.cells[x][y]
+func (b *Board) fillRandomEmptyCell() {
+	x, y := b.getRandomEmtpyCell()
+	b.set(x, y, GetStartValue())
 }
 
-func (b *BasicBoard) Set(x, y, v int) {
-	b.cells[x][y] = v
+func (b *Board) collapse(v TupleVector) (score int, did_something bool) {
+	// recursion exit point
+	//	{
+	//		fmt.Print("v=")
+	//		for _, t := range v {
+	//			fmt.Print(b.get(t.xy())," ")
+	//		}
+	//		fmt.Println()
+	//	}
+	l := len(v)
+	if l == 1 {
+		fmt.Println("len=1 => exit", v)
+		return 0, false
+	}
+
+	// If first emelent zero, look for the first non zero element and move it
+	// there. If non found recursion end.
+	x0, y0 := v[0].xy()
+	v0 := b.get(x0, y0)
+	if v0 == 0 {
+		for i := 1; i < l; i++ {
+			xi, yi := v[i].xy()
+			vi := b.get(xi, yi)
+			if vi != 0 {
+				fmt.Println("move non zero ", vi)
+				b.set(x0, y0, vi)
+				b.set(xi, yi, 0)
+				score, _ = b.collapse(v[:])
+				return score, true
+			}
+		}
+		//		fmt.Println("All zeros => bailout")
+		return 0, false
+	}
+	// ...else look for the next non zero element if its the same as v0
+	// combine them
+	for i := 1; i < l; i++ {
+		xi, yi := v[i].xy()
+		vi := b.get(xi, yi)
+		if vi == 0 {
+			continue
+		}
+		if v0 == vi {
+			fmt.Println("merge ", vi)
+			b.set(x0, y0, vi*2)
+			b.set(xi, yi, 0)
+			score, _ = b.collapse(v[1:])
+			return score + vi*2, true
+		}
+		break
+	}
+	return b.collapse(v[1:])
 }
 
+func (b *Board) goDir(dir string) (score int, done_something bool) {
+	
+	done_something = false
+	score = 0
+	tvv, _ := direction_map[dir]
+	for _, tv := range tvv {
+		s, d := b.collapse(tv)
+		score += s
+		done_something = done_something || d
+	}
+	return score, done_something
+}
+
+func (b *Board) left() (score int, done_something bool) {
+	return b.goDir("left")
+}
+
+func (b *Board) right() (score int, done_something bool) {
+	return b.goDir("right")
+}
+
+func (b *Board) up() (score int, done_something bool) {
+	return b.goDir("up")
+}
+
+func (b *Board) down() (score int, done_something bool) {
+	return b.goDir("down")
+}
 func main() {
-	var b *BasicBoard = new(BasicBoard)
-	BoardPrint(b)
-	empty := BoardGetEmpty(b)
-	fmt.Println(empty, len(empty))
+	rand.Seed(5)
+	var b *Board = new(Board)
+	keep_going := true
+	score := 0
+	tscore := 0
+	for keep_going {
+		tscore += score
+		fmt.Println("score", tscore)
+		b.fillRandomEmptyCell()
+		b.print()
+		score, keep_going = b.down()
+		if keep_going {
+			fmt.Println("down")
+			continue
+		}
+		score, keep_going = b.left()
+		if keep_going {
+			fmt.Println("left")
+			continue
+		}
+		score, keep_going = b.right()
+		if keep_going {
+			fmt.Println("right")
+			continue
+		}		 
+		score, keep_going = b.up()
+		if keep_going {
+			fmt.Println("up")
+			continue
+		}		 
+				 
+	}
 }
